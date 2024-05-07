35133320d647ced984a441c2dbb9856b2f9c1eda
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-05-07 14:21:38 -0400

    self-host satoshi fonts, create main axes lines for sphere

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,57 @@
 <script setup>
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { onMounted, reactive, ref } from 'vue'
-import WebGL from 'three/addons/capabilities/WebGL.js'
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls, Line2 } from '@tresjs/cientos'
 
-const threeMountPoint = ref(null)
+// const scene = new THREE.Scene()
+// const camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 1000)
+// camera.position.set(2, 0, 0)
+// camera.lookAt(0, 0, 0)
+// camera.rotation.z += Math.PI / 2
+// camera.rotation.y -= Math.PI / 2.5 - Math.PI / 2
+// // camera.setRotationFromAxisAngle(THREE.Vector3(0, 0, 1), Math.PI / 2)
 
-const width = 1280
-const height = 720
+// onMounted(() => {
+//   if (!WebGL.isWebGLAvailable()) {
+//     const warning = WebGL.getWebGLErrorMessage()
+//     threeMountPoint.value.innerText = warning
+//     return
+//   }
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 1000)
-camera.position.set(2, 0, 0)
-camera.lookAt(0, 0, 0)
-camera.rotation.z += Math.PI / 2
-camera.rotation.y -= Math.PI / 2.5 - Math.PI / 2
-// camera.setRotationFromAxisAngle(THREE.Vector3(0, 0, 1), Math.PI / 2)
+//   const renderer = new THREE.WebGLRenderer({ alpha: true })
+//   renderer.setSize(width, height)
+//   function animate() {
+//     requestAnimationFrame(animate)
+//     sphere.rotation.x += 0.01
+//     sphere.rotation.y += 0.01
+//     renderer.render(scene, camera)
+//   }
+//   threeMountPoint.value.appendChild(renderer.domElement)
+//   animate()
+// })
 
-onMounted(() => {
-  if (!WebGL.isWebGLAvailable()) {
-    const warning = WebGL.getWebGLErrorMessage()
-    threeMountPoint.value.innerText = warning
-    return
-  }
+// const geometry = new THREE.SphereGeometry(1)
+// const material = new THREE.MeshBasicMaterial({ color: 0x7b97f9, transparent: true, opacity: 0.4 })
+// const sphere = new THREE.Mesh(geometry, material)
+// // scene.add(sphere)
 
-  const renderer = new THREE.WebGLRenderer({ alpha: true })
-  renderer.setSize(width, height)
-  function animate() {
-    requestAnimationFrame(animate)
-    sphere.rotation.x += 0.01
-    sphere.rotation.y += 0.01
-    renderer.render(scene, camera)
-  }
-  threeMountPoint.value.appendChild(renderer.domElement)
-  animate()
-})
+// let lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff, linewidth: 5 })
+// let points = [new THREE.Vector3(0, 0, -1), new THREE.Vector3(0, 0, 1)]
+// let lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
+// const zAxis = new THREE.Line(lineGeometry, lineMaterial)
+// // line.computeLineDistances()
+// scene.add(zAxis)
 
-const geometry = new THREE.SphereGeometry(1)
-const material = new THREE.MeshBasicMaterial({ color: 0x7b97f9, transparent: true, opacity: 0.4 })
-const sphere = new THREE.Mesh(geometry, material)
-// scene.add(sphere)
+// lineMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00, linewidth: 5 })
+// points = [new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0)]
+// lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
+// const yAxis = new THREE.Line(lineGeometry, lineMaterial)
+// scene.add(yAxis)
 
-let lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff, linewidth: 5 })
-let points = [new THREE.Vector3(0, 0, -1), new THREE.Vector3(0, 0, 1)]
-let lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-const zAxis = new THREE.Line(lineGeometry, lineMaterial)
-// line.computeLineDistances()
-scene.add(zAxis)
-
-lineMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00, linewidth: 5 })
-points = [new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0)]
-lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-const yAxis = new THREE.Line(lineGeometry, lineMaterial)
-scene.add(yAxis)
-
-lineMaterial = new THREE.LineBasicMaterial({ color: 0xff0000, linewidth: 5 })
-points = [new THREE.Vector3(-1, 0, 0), new THREE.Vector3(1, 0, 0)]
-lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-const xAxis = new THREE.Line(lineGeometry, lineMaterial)
-scene.add(xAxis)
+// lineMaterial = new THREE.LineBasicMaterial({ color: 0xff0000, linewidth: 5 })
+// points = [new THREE.Vector3(-1, 0, 0), new THREE.Vector3(1, 0, 0)]
+// lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
+// const xAxis = new THREE.Line(lineGeometry, lineMaterial)
+// scene.add(xAxis)
 </script>
 
 <template>
@@ -67,12 +60,50 @@ scene.add(xAxis)
   </header>
 
   <main>
-    <div ref="threeMountPoint"></div>
+    <div id="tres-canvas">
+      <TresCanvas :alpha="true">
+        <TresPerspectiveCamera :up="[0, 0, 1]" :position="[2, 1, 1]" :look-at="[0, 0, 0]" />
+        <OrbitControls />
+        <TresMesh>
+          <TresSphereGeometry :args="[1]" />
+          <TresMeshBasicMaterial :color="0x7b97f9" :transparent="true" :opacity="0.4" />
+        </TresMesh>
+        <!-- <TresAxesHelper /> -->
+        <Line2
+          :points="[
+            [-1, 0, 0],
+            [1, 0, 0]
+          ]"
+          :line-width="10"
+          color="#ff0000"
+        />
+        <Line2
+          :points="[
+            [0, -1, 0],
+            [0, 1, 0]
+          ]"
+          :line-width="10"
+          color="#00ff00"
+        />
+        <Line2
+          :points="[
+            [0, 0, -1],
+            [0, 0, 1]
+          ]"
+          :line-width="10"
+          color="#0000ff"
+        />
+      </TresCanvas>
+    </div>
   </main>
 </template>
 
 <style scoped>
 h1 {
   font-weight: bold;
+}
+#tres-canvas {
+  width: 1280px;
+  height: 720px;
 }
 </style>
