2da5e0b3f7e927e408cc9959c7c91076da7ceec2
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-05-08 21:46:15 -0400

    fix performance issue with mouse click on sphere using tres v4.0.0 beta

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <script setup>
 import { TresCanvas } from '@tresjs/core'
-import { OrbitControls } from '@tresjs/cientos'
+import { Line2, OrbitControls, Stats } from '@tresjs/cientos'
 import AxesLines from './components/AxesLines.vue'
 import AxesLabels from './components/AxesLabels.vue'
+import { computed, onMounted, shallowRef } from 'vue'
+import { Vector3 } from 'three'
+
+const qubitPosition = shallowRef(new Vector3(0, 0, 1))
+const canvasRef = shallowRef(null)
+
+function handlePointerDown(intersection) {
+  qubitPosition.value = intersection.point
+}
+
+const qubitLinePoints = computed(() => {
+  return [[0, 0, 0], qubitPosition.value]
+})
+
+onMounted(() => {
+  console.log('canvasRef.value.context', canvasRef.value.context.raycaster)
+})
 </script>
 
 <template>
@@ -12,19 +29,28 @@ import AxesLabels from './components/AxesLabels.vue'
 
   <main>
     <div id="tres-canvas">
-      <TresCanvas :alpha="true">
-        <TresPerspectiveCamera :up="[0, 0, 1]" :position="[3, 1, 1]" :look-at="[0, 0, 0]" />
+      <TresCanvas :alpha="true" ref="canvasRef">
+        <TresPerspectiveCamera
+          :up="[0, 0, 1]"
+          :position="[3, 1, 1]"
+          :look-at="[0, 0, 0]"
+          :near="0.1"
+          :far="1000"
+        />
         <OrbitControls />
-        <TresMesh>
-          <TresSphereGeometry :args="[1]" />
+        <Stats />
+
+        <TresMesh :position="[0, 0, 0]" @pointer-down="handlePointerDown">
+          <TresSphereGeometry :args="[1, 64, 32]" />
           <TresMeshBasicMaterial :color="0x7b97f9" :transparent="true" :opacity="0.4" />
         </TresMesh>
 
         <AxesLines />
-
         <Suspense>
           <AxesLabels />
         </Suspense>
+
+        <Line2 :points="qubitLinePoints" :line-width="5" />
       </TresCanvas>
     </div>
   </main>
