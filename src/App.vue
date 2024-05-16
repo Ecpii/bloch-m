8338437048291b26604250199ea3b32fdc91f33d
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-05-16 15:16:11 -0400

    show rotation with rotating axes copies

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { computed, shallowRef, triggerRef } from 'vue'
 import { TresCanvas, useRenderLoop } from '@tresjs/core'
 import { Line2, OrbitControls, Sphere } from '@tresjs/cientos'
-import { Vector3 } from 'three'
+import { Vector3, BufferGeometry, Line, LineBasicMaterial, Object3D, Euler } from 'three'
 import {
   GATES,
   calculateStatevector,
@@ -17,11 +17,14 @@ import StateDisplay from './components/StateDisplay.vue'
 import AxesLines from './components/AxesLines.vue'
 import AxesLabels from './components/AxesLabels.vue'
 import AnimationSettings from './components/AnimationSettings.vue'
+Object3D.DEFAULT_UP = new Vector3(0, 0, 1) // change to z-up system
 
 const qubitPosition = shallowRef(new Vector3(0, 0, 1))
 const currentGate = shallowRef(null)
 const hoveredGate = shallowRef(null)
-const animationDuration = shallowRef(0.4)
+const animationDuration = shallowRef(1) // in seconds
+const lineRef = shallowRef(null)
+const sphereRef = shallowRef(null)
 const { onLoop } = useRenderLoop()
 
 function handlePointerDown(intersection) {
@@ -55,13 +58,36 @@ function handleRotationGate(key, axis, angle) {
   fireGate(newGate)
 }
 
+function createAxisCopies() {
+  // console.log('lineRef.value.rotation', lineRef.value.rotation)
+  // const material = new LineBasicMaterial({ color: 0x7b97f9 })
+  // const xAxisPoints = [new Vector3(-1, 0, 0), new Vector3(1, 0, 0)]
+  // const yAxisPoints = [new Vector3(0, -1, 0), new Vector3(0, 1, 0)]
+  // const zAxisPoints = [new Vector3(0, 0, -1), new Vector3(0, 0, 1)]
+  // const xAxisGeometry = new BufferGeometry().setFromPoints(xAxisPoints)
+  // const yAxisGeometry = new BufferGeometry().setFromPoints(yAxisPoints)
+  // const zAxisGeometry = new BufferGeometry().setFromPoints(zAxisPoints)
+  // lineRef.value.add(new Line(xAxisGeometry, material))
+  // lineRef.value.add(new Line(yAxisGeometry, material))
+  // lineRef.value.add(new Line(zAxisGeometry, material))
+  lineRef.value.visible = true
+}
+
+function removeAxisCopies() {
+  lineRef.value.visible = false
+  // lineRef.value.clear()
+  lineRef.value.setRotationFromEuler(new Euler())
+}
+
 function fireGate(gate) {
   const originalStatevector = qubitStatevector.value
   const endStatevector = applyGate(originalStatevector, gate)
+  createAxisCopies()
   currentGate.value = gate
   setTimeout(() => {
     currentGate.value = null
     setQubitStatevector(endStatevector)
+    setTimeout(removeAxisCopies, 500)
   }, animationDuration.value * 1000)
 }
 function setQubitStatevector(newStatevector) {
@@ -83,6 +109,8 @@ onLoop(({ delta }) => {
   if (currentGate.value !== null) {
     const angle = (delta / animationDuration.value) * currentGate.value.rotation
     qubitPosition.value.applyAxisAngle(currentGate.value.axis[0], angle)
+    lineRef.value.rotateOnWorldAxis(currentGate.value.axis[0], angle)
+    sphereRef.value.rotateOnWorldAxis(currentGate.value.axis[0], angle)
     // without this triggerRef the qubitPosition line will not animate
     triggerRef(qubitPosition)
   }
@@ -99,16 +127,41 @@ onLoop(({ delta }) => {
         :near="0.1"
         :far="100"
       />
+      <!-- <TresAmbientLight :intensity="1" />
+      <TresDirectionalLight cast-shadow :position="[1, 1, 1]" :intensity="10" /> -->
+
       <OrbitControls :enable-zoom="false" />
 
       <AxesLines />
-      <Suspense>
-        <AxesLabels />
-      </Suspense>
+      <AxesLabels />
 
-      <TresMesh :position="[0, 0, 0]" @pointer-down="handlePointerDown">
+      <TresGroup ref="lineRef" :up="[0, 0, 1]" :visible="false">
+        <Line2
+          :points="[
+            [0, 0, -1],
+            [0, 0, 1]
+          ]"
+          color="#7b97f9"
+        />
+        <Line2
+          :points="[
+            [0, -1, 0],
+            [0, 1, 0]
+          ]"
+          color="#7b97f9"
+        />
+        <Line2
+          :points="[
+            [-1, 0, 0],
+            [1, 0, 0]
+          ]"
+          color="#7b97f9"
+        />
+      </TresGroup>
+
+      <TresMesh :position="[0, 0, 0]" @pointer-down="handlePointerDown" ref="sphereRef">
         <TresSphereGeometry :args="[1, 64, 32]" />
-        <TresMeshBasicMaterial color="#7b97f9" :transparent="true" :opacity="0.2" />
+        <TresMeshStandardMaterial color="#7b97f9" :transparent="true" :opacity="0.2" />
       </TresMesh>
 
       <Sphere :args="[0.015]" :position="qubitPosition" color="#cfb805" />
