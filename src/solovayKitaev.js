6f983f7add3a1085e27289145b79640541321fae
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-05-22 11:43:35 -0400

    create algorithm to find closest basic approximation

--- src/solovayKitaev.js
+++ src/solovayKitaev.js
@@ -1,4 +1,4 @@
-import { matrix, divide, sqrt } from 'mathjs'
+import { matrix, divide, sqrt, subtract, norm } from 'mathjs'
 import basicApproximations from './assets/basicApproximations.json'
 const GATE_MATRICES = {
   h: divide(
@@ -20,12 +20,21 @@ class GateSequence {
 
 export function solovayKitaev(target, n) {
   if (n === 0) {
-    console.log('basicApproximations', basicApproximations)
-    console.log('basicApproximations[0]', basicApproximations[0])
+    return findClosestBasicApproximation(target)
   }
 }
 
-// function findClosestBasicApproximation(target) {
-//   const targetMatrix = target.product;
-
-// }
+function findClosestBasicApproximation(target) {
+  const targetMatrix = target.product
+  let minLoss = Infinity
+  let minLossSequence
+  for (const sequence of basicApproximations) {
+    const sequenceMatrix = matrix(sequence.matrix)
+    const loss = norm(subtract(sequenceMatrix, targetMatrix), 'fro')
+    if (loss < minLoss) {
+      minLoss = loss
+      minLossSequence = sequence
+    }
+  }
+  return minLossSequence
+}
