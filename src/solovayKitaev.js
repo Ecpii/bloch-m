40dbffc2690b612fb437df410a11391224b33e34
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-05-23 00:13:28 -0400

    add clean function to gatesequence

--- src/solovayKitaev.js
+++ src/solovayKitaev.js
@@ -7,19 +7,17 @@ import {
   multiply,
   norm,
   inv,
-  eigs,
   acos,
   trace,
   asin,
   nthRoot,
   cos,
   sin,
-  transpose,
-  index,
   dot,
   cross,
   identity,
-  add
+  add,
+  det
 } from 'mathjs'
 import basicApproximations from './assets/basicApproximations.json'
 const GATE_MATRICES = {
@@ -34,10 +32,19 @@ const GATE_MATRICES = {
 class GateSequence {
   gates = []
   matrices = []
-  product
-  // constructor() {
+  product = identity(3)
 
-  // }
+  static fromsu2Matrix(matrix) {
+    const res = new GateSequence()
+    res.product = convertSu2ToSo3(matrix)
+    return res
+  }
+  static fromso3Matrix(matrix) {
+    const res = new GateSequence()
+    res.product = matrix
+    return res
+  }
+
   adjoint() {
     const res = new GateSequence()
     for (const matrix of this.matrices.toReversed()) {
@@ -55,14 +62,73 @@ class GateSequence {
     res.product = ctranspose(this.product)
     return res
   }
+  dot(other) {
+    const res = new GateSequence()
+    res.gates = other.gates.concat(this.gates)
+    res.matrices = other.matrices.concat(this.matrices)
+    res.product = dot(this.product, other.product)
+    return res
+  }
+
+  clean() {
+    let index = 0
+    while (index < this.gates.length - 1) {
+      const currentName = this.gates[index]
+      const nextName = this.gates[index + 1]
+      if (nextName === inverseGateName(currentName)) {
+        this.gates.splice(index, 2)
+        // check if this removal caused another inverse neighbor
+        index--
+      }
+      index++
+    }
+  }
 }
 
-export function solovayKitaev(target, n) {
-  if (n === 0) {
-    return findClosestBasicApproximation(target)
+function inverseGateName(gateName) {
+  if (gateName === 'h') {
+    return 'h'
+  } else if (gateName === 't') {
+    return 'tdg'
+  } else if (gateName === 'tdg') {
+    return 't'
   }
-  const previousLevelApprox = solovayKitaev(target, n - 1)
-  const delta = target.dot(previousLevelApprox.adjoint())
+}
+
+function convertSu2ToSo3(mat) {
+  const a = mat.get([0, 0])?.re ?? mat.get([0, 0])
+  const b = mat.get([0, 0])?.im ?? 0
+  const c = -1 * (mat.get([0, 1])?.re ?? mat.get([0, 1]))
+  const d = -1 * (mat.get([0, 1])?.im ?? 0)
+  return matrix([
+    [a ** 2 - b ** 2 - c ** 2 + d ** 2, 2 * a * b + 2 * c * d, -2 * a * c + 2 * b * d],
+    [-2 * a * b + 2 * c * d, a ** 2 - b ** 2 + c ** 2 - d ** 2, 2 * a * d + 2 * b * c],
+    [2 * a * c + 2 * b * d, 2 * b * c - 2 * a * d, a ** 2 + b ** 2 - c ** 2 - d ** 2]
+  ])
+}
+
+export function solovayKitaevFromU2(targetMatrix, n) {
+  const phaseFactor = 1 / sqrt(det(targetMatrix))
+  const inputSequence = GateSequence.fromsu2Matrix(multiply(phaseFactor, targetMatrix))
+  // const globalPhase = atan2(phaseFactor.im, phaseFactor.re)
+
+  const resSequence = solovayKitaev(inputSequence, n)
+  resSequence.clean()
+}
+
+function solovayKitaev(u, n) {
+  if (n === 0) {
+    return findClosestBasicApproximation(u)
+  }
+  const uApprox = solovayKitaev(u, n - 1)
+  const delta = u.dot(uApprox.adjoint())
+
+  const [v, w] = balancedCommutatorDecomposition(delta)
+
+  const vApprox = solovayKitaev(v, n - 1)
+  const wApprox = solovayKitaev(w, n - 1)
+
+  return vApprox.dot(wApprox).dot(vApprox.adjoint()).dot(wApprox.adjoint()).dot(uApprox)
 }
 
 function findClosestBasicApproximation(target) {
@@ -145,5 +211,5 @@ function balancedCommutatorDecomposition(so3Matrix) {
   const v = dot(dot(simMatrix, vTilde), simMatrixdg)
   const w = dot(dot(simMatrix, wTilde), simMatrixdg)
 
-  return v, w
+  return [GateSequence.fromso3Matrix(v), GateSequence.fromso3Matrix(w)]
 }
