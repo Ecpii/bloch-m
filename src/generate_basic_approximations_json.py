34f2a2be425f98b94ff9a4e50a0935dd449d9030
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-06-14 13:16:34 -0400

    save global phase on approximations

--- src/generate_basic_approximations_json.py
+++ src/generate_basic_approximations_json.py
@@ -13,23 +13,35 @@ def remove_whitespace(string):
     return string.translate(whitespace_table)
 
 
+def format_gateseq(gateseq):
+    gate_names = clean_gates(gateseq.gates)
+    gate_matrix = clean_matrix(gateseq.product)
+    phase = gateseq.global_phase
+    return f'{{"names":{gate_names},"matrix":{gate_matrix},"phase":{phase}}}'
+
+
+def clean_gates(gates):
+    return remove_whitespace(
+        str([gate.name for gate in gates]).replace("'", '"')
+    )
+
+
+def clean_matrix(matrix):
+    matrix = remove_whitespace(repr(matrix)[6:-1])
+    matrix = matrix.replace(".,", ",")
+    matrix = matrix.replace(".]", "]")
+    matrix = matrix.replace(".00000000e+00", "")
+    return matrix
+
+
 res = generate_basic_approximations(["h", "t", "tdg"], 10)
 print("[", end="")
 for gateseq in res[:-1]:
-    gate_names = remove_whitespace(
-        str(list(map(lambda gate: gate.name, gateseq.gates))).replace("'", '"')
+    print(
+        format_gateseq(gateseq),
+        end=",",
     )
-    gate_matrix = remove_whitespace(repr(gateseq.product)[6:-1])
-    gate_matrix = gate_matrix.replace("0.,", "0,")
-    gate_matrix = gate_matrix.replace("1.,", "1,")
-    gate_matrix = gate_matrix.replace("0.]", "0]")
-    gate_matrix = gate_matrix.replace("1.]", "1]")
-    print(f'{{"names":{gate_names},"matrix":{gate_matrix}}},', end="")
 
 gateseq = res[-1]
-gate_names = remove_whitespace(
-    str(list(map(lambda gate: gate.name, gateseq.gates))).replace("'", '"')
-)
-gate_matrix = remove_whitespace(repr(gateseq.product)[6:-1])
-print(f'{{"names":{gate_names},"matrix":{gate_matrix}}}', end="")
+print(format_gateseq(gateseq), end="")
 print("]", end="")
